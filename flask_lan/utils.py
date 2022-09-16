f64f51da99515aed74bb04877ad12a8bdec9617d
ischaojie <zhuzhezhe95@gmail.com>
2022-09-16 23:38:14 +0800

    fix: check func param is required

--- flask_lan/utils.py
+++ flask_lan/utils.py
@@ -19,3 +19,12 @@ def get_f_defaults(f: Callable):
     for k, v in sig.parameters.items():
         if v.default is not Parameter.empty:
             yield k, v.default
+
+
+def is_f_param_required(f: Callable, param_name: str) -> bool:
+    sig = signature(f)
+    params_dict = dict((sig.parameters.items()))
+    parameter = params_dict.get(param_name, None)
+    if not parameter:
+        return False
+    return parameter.default is parameter.empty
