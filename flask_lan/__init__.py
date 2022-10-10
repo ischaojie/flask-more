fd6961c2ab03b486c7d66b3dedcc2b5d14bc955a
ischaojie <zhuzhezhe95@gmail.com>
2022-10-10 18:05:18 +0800

    v0.1.9

--- flask_lan/__init__.py
+++ flask_lan/__init__.py
@@ -2,5 +2,5 @@ from flask_lan.core import Lan
 from flask_lan.openapi import docs
 from flask_lan.validate import validator
 
-__version__ = "0.1.5"
+__version__ = "0.1.9"
 __all__ = ["Lan", "validator", "docs"]
