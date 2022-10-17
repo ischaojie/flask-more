1a29ace4e259e346b9b08303a3d8bc5fc01b6699
ischaojie <zhuzhezhe95@gmail.com>
2022-10-17 11:14:44 +0800

    docs: change example and swagger show

--- example/app.py
+++ example/app.py
@@ -7,15 +7,69 @@ app = Flask(__name__)
 
 Lan(app, "Book API")
 
+books = [
+    {
+        "title": "The Three-Body Problem",
+        "author": "liucixin",
+        "price": 10.5,
+    },
+    {
+        "title": "The Kite Runner",
+        "author": "Khaled Hosseini",
+        "price": 22,
+    },
+]
+
 
 class BookSchema(BaseModel):
     title: str
+    author: str
     price: float
 
-@app.get("/books/<id>")
-@api(tags=["book"], summary="book example")
-def example(id: int, hi: str, book: BookSchema):
-    return {"id": id, "hi": hi, "book": book.dict()}
+
+@app.get("/")
+@api
+def home():
+    return {"msg": "hello"}
+
+
+@app.post("/books")
+@api(tags=["books"], summary="create book")
+def create_book(schema: BookSchema):
+    new_book = schema.dict()
+    books.append(new_book)
+    return new_book
+
+
+@app.get("/books")
+@api(
+    tags=["books"],
+    summary="query book",
+    description="query book by some search condition",
+)
+def query_book(like: str = "", start: int = 0, count: int = 10):
+    r = list(filter(lambda book: like in book["title"], books))
+    return {
+        "start": start,
+        "count": count,
+        "items": r,
+        "total": len(r),
+    }
+
+
+@app.get("/books/<title>")
+@api(
+    tags=["books"],
+    summary="get book",
+    description="get book by title",
+)
+def get_book(title: str):
+    r = {}
+    for book in books:
+        if book["title"] == title:
+            r = book
+    return r
+
 
 if __name__ == "__main__":
     app.run(debug=True)
