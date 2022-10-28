3ebecbbd2beb013de503113fd02b93183c29f528
chaojie <hi@chaojie.fun>
2022-10-28 17:20:30 +0800

    chore: rename api to wraps

--- flask_lan/__init__.py
+++ flask_lan/__init__.py
@@ -1,6 +1,6 @@
-from flask_lan.api import api
 from flask_lan.core import Lan
 from flask_lan.validate import validator
+from flask_lan.wraps import api
 
 __version__ = "0.1.11"
 __all__ = ["Lan", "api", "validator"]
