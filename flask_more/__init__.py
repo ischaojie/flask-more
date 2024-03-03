b7e75ac57d9a6422a608cf897c4ef35b4a5e464f
Chaojie <hi@chaojie.fun>
2024-03-04 00:15:52 +0800

    Update (#44)

--- flask_more/__init__.py
+++ flask_more/__init__.py
@@ -2,5 +2,5 @@ from flask_more.core import More
 from flask_more.validate import validator
 from flask_more.wraps import api
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 __all__ = ["More", "api", "validator"]
